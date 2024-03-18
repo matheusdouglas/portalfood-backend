c5154b662ba8958e4310c272ac5b4e094040d261
unknown <matheusdouglas1001@gmail.com>
2024-03-18 16:26:23 -0300

    Enviando novas alteracoes

--- prisma/migrations/20231028174316_created_entidies/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `plate` to the `students` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "students" ADD COLUMN     "plate" TEXT NOT NULL;
